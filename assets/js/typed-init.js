cad17663903e65e4e32caa315c9a2579408fa1aa
Chris Motola <cmotola@gmail.com>
2025-08-12 15:35:41 -0400

    Update typed-init.js

--- assets/js/typed-init.js
+++ assets/js/typed-init.js
@@ -1,12 +1,15 @@
-new Typed('#typed-text', {
-  strings: ["Data into stories", "Stories into systems", "Patterns in the noise"],
-  typeSpeed: 50,
-  backSpeed: 25,
-  backDelay: 1500,
-  loop: true,
-  showCursor: true,
-  cursorChar: '|'
+document.addEventListener('DOMContentLoaded', () =>{
+  new Typed('#typed-text', {
+    strings: ["Data into stories", "Stories into systems", "Patterns in the noise"],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1500,
+    loop: true,
+    showCursor: true,
+    cursorChar: '|'
+  });
 });
+
 
 
       
