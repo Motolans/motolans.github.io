1ac69744d199cc4db21211fde02ce850c41a0511
Chris Motola <cmotola@gmail.com>
2025-08-12 12:58:24 -0400

    Update typed-init.js

--- assets/js/typed-init.js
+++ assets/js/typed-init.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
   new Typed("#typed-text", {
     strings: [
-      "I turn data into stories.",
-      "I solve complex problems.",
-      "I find patterns in the noise."
+      "Data into stories",
+      "Stories into systems",
+      "Patterns in the noise"
     ],
     typeSpeed: 50,
     backSpeed: 25,
