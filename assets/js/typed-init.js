928797aed1556b063a8e0ccdb8774b91b415403d
Chris Motola <cmotola@gmail.com>
2025-08-12 15:16:37 -0400

    Update typed-init.js

--- assets/js/typed-init.js
+++ assets/js/typed-init.js
@@ -2,7 +2,7 @@ const typedEl = document.getElementById('typed-text');
 typedEl.style.visibility = 'hidden';
 
 const typed = new Typed('#typed-text', {
-  strings: ["Data into stories", "Stories into systems"],
+  strings: ["Data into stories", "Stories into systems", "Patterns in the noise"],
   typeSpeed: 50,
   backSpeed: 25,
   backDelay: 1000,
