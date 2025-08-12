54ad001b3b039c0fc80a04edcd0566ff8fc79178
Chris Motola <cmotola@gmail.com>
2025-08-12 11:32:11 -0400

    Create typed-init.js

---
+++ assets/js/typed-init.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+  new Typed("#typed-text", {
+    strings: [
+      "I turn data into stories.",
+      "I solve complex problems.",
+      "I find patterns in the noise."
+    ],
+    typeSpeed: 50,
+    backSpeed: 25,
+    backDelay: 1500,
+    loop: true
+  });
+});
